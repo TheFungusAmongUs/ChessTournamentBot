d0004453cc205483838a180e7e291a8d1ff4d13d
Nathan Pfeffer <nathan.pfeffer@protonmail.com>
2022-03-19 13:24:47 -0400

    Add util function to recursively convert model to dict

--- utils/file_handling.py
+++ utils/file_handling.py
@@ -31,3 +31,10 @@ def write_toml(to_write: toml_dict, fp: TextIO) -> None:
     fp.truncate()
     toml.dump(to_write, fp)
     fp.seek(0)
+
+
+# Attribution: Archit Dwidedi, Stack Overflow: https://stackoverflow.com/a/49003922/16177247
+def get_json(obj):
+    return json.loads(
+        json.dumps(obj, default=lambda o: getattr(o, '__dict__', str(o)))
+    )
