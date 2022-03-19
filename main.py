26fd438386f84b9c7d738407f1a02c3d84d1e8ea
Nathan Pfeffer <nathan.pfeffer@protonmail.com>
2022-03-19 01:07:41 -0400

    Rename GTAHSBot to ChessTournamentBot

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import toml
 from discord.ext import commands
 
 
-class GTAHSBot(commands.Bot):
+class ChessTournamentBot(commands.Bot):
 
     def __init__(self):
         super().__init__(command_prefix='', help_command=None)
 
 
 async def main():
-    bot = GTAHSBot()
+    bot = ChessTournamentBot()
     async with bot:
         await bot.start(toml.load("config/config.toml")["TOKEN"])
 
