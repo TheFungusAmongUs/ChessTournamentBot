ac2a971d93a48571b4cdbdbe4e99269ef2007a41
Nathan Pfeffer <nathan.pfeffer@protonmail.com>
2022-03-19 12:51:04 -0400

    Split slash command into a Cog

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import asyncio
 import nextcord
 import pandas
 import toml
-from nextcord.ext import application_checks, commands
+from nextcord.ext import commands
+from cogs.excel_handling import ExcelCog
 
-
-class ChessTournamentBot(commands.Bot):
-
-    SCHOOLS: dict[str, pandas.DataFrame] = {
+SCHOOLS: dict[str, pandas.DataFrame] = {
         "West Humber": ...,
         "Saint Andrew's": ...,
         "Earl Haig": ...,
@@ -15,25 +12,19 @@ class ChessTournamentBot(commands.Bot):
         "Richview": ...
     }
 
+
+class ChessTournamentBot(commands.Bot):
+
     def __init__(self):
         super().__init__(command_prefix='', help_command=None, intents=nextcord.Intents.all())
 
-    def on_ready(self):
+    async def on_ready(self):
         print(nextcord.__version__)
-
-    @nextcord.slash_command(
-        name="load_users",
-        description="loads users from a spreadsheet",
-        guild_ids=[951146346335862844]
-    )
-    @application_checks.is_owner()
-    async def load_users(self, interaction: nextcord.Interaction):
-        for school, df in self.SCHOOLS.items():
-            df = pandas.read_excel(f"data/{school}.xlsx", engine="openpyxl")
 
 
 def main():
     bot = ChessTournamentBot()
+    bot.add_cog(ExcelCog(bot, SCHOOLS))
     bot.run(toml.load("config/config.toml")["TOKEN"])
 
 
