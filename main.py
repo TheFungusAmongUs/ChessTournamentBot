2ff5a5abbca55519ea64dcf91121313c836c10c7
Nathan Pfeffer <nathan.pfeffer@protonmail.com>
2022-03-19 02:16:47 -0400

    Switch from discord.py to nextcord because discord.py app_commands are brutal

--- main.py
+++ main.py
@@ -11,11 +11,10 @@ class ChessTournamentBot(commands.Bot):
         super().__init__(command_prefix='', help_command=None, intents=nextcord.Intents.all())
 
 
-async def main():
+def main():
     bot = ChessTournamentBot()
-    async with bot:
-        await bot.start(toml.load("config/config.toml")["TOKEN"])
+    bot.run(toml.load("config/config.toml")["TOKEN"])
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
