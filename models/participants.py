2f8a8f2ae36bd762745d81338886c78483eab558
Nathan Pfeffer <nathan.pfeffer@protonmail.com>
2022-03-25 09:46:17 -0400

    Make channel and member ints, add matchup field and methods to get Member and Channel objects from ids.

--- models/participants.py
+++ models/participants.py
@@ -1,11 +1,23 @@
 import nextcord
-from stats import Stats
-from dataclasses import dataclass
+from models.stats import Stats
+from dataclasses import dataclass, field
+from typing import Optional
 
 
 @dataclass()
 class Participant:
 
-    channel: nextcord.TextChannel
-    stats: Stats
+    member: int
+    name: str
     school: str
+    stats: Stats
+    sub: bool
+    username: str
+    channel: Optional[int] = None
+    matchups: Optional[list['Participant']] = field(default_factory=lambda: [None, None, None, None, None])
+
+    def get_member(self, guild) -> nextcord.Member:
+        return guild.get_member(self.member)
+
+    def get_channel(self, bot) -> nextcord.TextChannel:
+        return bot.get_channel(self.channel)
