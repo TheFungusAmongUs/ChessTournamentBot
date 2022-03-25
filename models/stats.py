cd8b044445c8fc1fc5d450ffc0ad303bd88f0bd8
Nathan Pfeffer <nathan.pfeffer@protonmail.com>
2022-03-25 09:44:32 -0400

    Add default value for stats fields

--- models/stats.py
+++ models/stats.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 @dataclass()
 class Stats:
 
-    wins: int
-    draws: int
-    losses: int
+    wins: int = 0
+    draws: int = 0
+    losses: int = 0
 
     def total_points(self) -> float:
         return self.wins + self.draws * 0.5
